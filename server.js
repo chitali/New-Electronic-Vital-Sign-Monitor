761303110b5ec54f529a83b41a3d08bf981733a4
Chitali <bugec@oregonstate.edu>
2020-06-22 15:20:21 -0700

    templatized website using express and added different subpages

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get(['/home', '/'], function (req, res, next) {
+    res.render('home');
+});
+
+app.get('/oscillator', function (req, res, next) {
+  res.render('oscillator');
+
+});
+app.get('/instrument', function (req, res, next) {
+    res.render('instrument',{
+      instrument: true
+    });
+  
+});
+app.get('*', function (req, res, next) {
+    res.render('404');
+  
+  });
+
+app.listen(port, function () {
+    console.log("== Server is listening on port", port);
+});
